7c3600dc7816b2f60a08ec09af2a1c1292c3993b
rnons <remotenonsense@gmail.com>
2012-12-08 09:29:07 +0800

    *[notesEdit]escape QSyntaxHighlight in fenced_block

--- mikidown/highlighter.py
+++ mikidown/highlighter.py
@@ -2,55 +2,39 @@ from mikidown import *
 import re
 
 class MikiHighlighter(QSyntaxHighlighter):
+    
     def __init__(self, parent=None):
         super(MikiHighlighter, self).__init__(parent)
         baseFontSize = 12
-        NUM = 14
+        NUM = 15
         self.patterns = []
         regexp = [0] * NUM
         font = [0]*NUM
         color = [0]*NUM
         # 0: html tags - <pre></pre>
         regexp[0] = '</?[0-z]+>'
-        #font[0] = QFont(None, baseFontSize, QFont.Bold)
-        font[0] = QFont(None, baseFontSize, -1)
-        #color[0] = QColor("#4E9A06")
+        font[0] = QFont("monospace", baseFontSize, -1)
         color[0] = QColor("#A40000")
-        font[0].setStyleHint(QFont.Courier)
         # 1: h1 - #
         regexp[1] = '^#[^#]+'
         color[1] = QColor("#4E9A06")
-        #color[1] = QColor("#CD5C5C")
-        #color[1] = QColor("#A40000")
-        font[1] = QFont(None, 2*baseFontSize, QFont.Bold)
-        #font[1].setStyleHint(QFont.Serif)
-        font[1].setStyleHint(QFont.Decorative)
+        font[1] = QFont("decorative", 2*baseFontSize, QFont.Bold)
         # 2: h2 - ##
         regexp[2] = '^##[^#]+'
-        #color[2] = QColor("#729FCF")
         color[2] = QColor("#4E9A06")
-        #color[2] = QColor("#73D216")
-        font[2] = QFont(None, 5.0/3*baseFontSize, QFont.Bold)
-        font[2].setStyleHint(QFont.Serif)
+        font[2] = QFont("serif", 5.0/3*baseFontSize, QFont.Bold)
         # 3: h3 - ###
         regexp[3] = '^###[^#]+'
-        #color[3] = QColor("#729FCF")
         color[3] = QColor("#4E9A06")
-        #color[3] = QColor("#73D216")
-        #font[3] = QFont(None, 4.0/3*baseFontSize, -1)
-        font[3] = QFont(None, 4.0/3*baseFontSize, QFont.Bold)
-        font[3].setStyleHint(QFont.Serif)
+        font[3] = QFont("serif", 4.0/3*baseFontSize, QFont.Bold)
         # 4: h4 and more - ####
         regexp[4] = '^####.+'
         color[4] = QColor("#4E9A06")
-        #color[4] = QColor("#729FCF")
-        font[4] = QFont(None, baseFontSize, QFont.Bold)
-        font[4].setStyleHint(QFont.Serif)
+        font[4] = QFont("serif", baseFontSize, QFont.Bold)
         # 5: html symbols - &gt;
         regexp[5] = '&[^; ].+;'
         color[5] = QColor("#A40000")
-        font[5] = QFont(None, baseFontSize, -1)
-        font[5].setStyleHint(QFont.Courier)
+        font[5] = QFont("monospace", baseFontSize, -1)
         # 6: html comments - <!-- -->
         regexp[6] = '<!--.+-->'
         color[6] = QColor("#888A85")
@@ -78,17 +62,16 @@ class MikiHighlighter(QSyntaxHighlighter):
         #.setUnderlineColor("#204A87")
         # 12: link/image references - [] or ![]
         regexp[12] = r'!?\[[^\[\]]*\]'
-        #color[12] = QColor("#3465A4")
         color[12] = QColor("#204A87")
-        #color[12] = QColor("#4183C4")
-        #color[12] = QColor("#CD5C5C")
-        #color[12] = QColor("#3377AA")
         font[12] = QFont(None, baseFontSize, -1)
         # 13: blockquotes and lists -  > or - or *
         regexp[13] = r'(^>+)|(^- )|(^\* )'
         color[13] = QColor("#F57900")
-        #color[13] = QColor("#A40000")
         font[13] = QFont(None, baseFontSize, -1)
+        # 14: fence - ``` or ~~~
+        regexp[14] = '^(?:~{3,}|`{3,}).*$'
+        color[14] = QColor("#F57900")
+        font[14] = QFont(None, baseFontSize, QFont.Bold)
         for i in range(NUM):
             p = re.compile(regexp[i])
             f = QTextCharFormat()
@@ -97,9 +80,29 @@ class MikiHighlighter(QSyntaxHighlighter):
             if color[i] != 0:
                 f.setForeground(color[i])
             self.patterns.append((p, f))
+
+        fenced_font = QFont("monospace", baseFontSize, -1)
+        self.fenced_block = re.compile("^(?:~{3,}|`{3,}).*$")
+        self.fenced_format = QTextCharFormat()
+        self.fenced_format.setFont(fenced_font)
     
     def highlightBlock(self, text):
+        # highlight patterns
         for i in range(0,len(self.patterns)):
             p = self.patterns[i]
             for match in p[0].finditer(text):
                 self.setFormat(match.start(), match.end() - match.start(), p[1])
+        
+        # escape highlights in fenced_block
+        m = self.fenced_block.match(text)
+        self.setCurrentBlockState(0)
+        if self.previousBlockState() != 1:
+            if m:
+                self.setCurrentBlockState(1)
+        else:
+            if m:
+                self.setCurrentBlockState(0)
+            else:
+                self.setCurrentBlockState(1)
+                self.setFormat(0, len(text), self.fenced_format)
+
