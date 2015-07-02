b8a486558de24ca93fb7a222ec82232713b6ca55
ShadowKyogre <shadowkyogre@aim.com>
2015-07-01 19:28:08 -0700

    Rough draft attempt at addressing #46, needs testing from reporter too

---
+++ mikidown/mdx_autodir.py
@@ -0,0 +1,65 @@
+"""
+AutoTextDirection Extension for Python-Markdown
+========================================
+
+With AutoTextDirection, #Header will become
+
+    <h1 id="header" dir="auto">Header</h1>
+"""
+
+from markdown import Extension
+from markdown.treeprocessors import Treeprocessor
+from markdown.util import etree
+
+import unicodedata as UD
+
+
+class AutoTextDirectionTreeprocessor(Treeprocessor):
+    """ Insert anchors to headers. """
+    BLOCK_LVL_ELS={'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'ul', 'ol'}
+    def run(self, doc):
+        for elem in doc.getiterator():
+            if elem.tag in AutoTextDirectionTreeprocessor.BLOCK_LVL_ELS:
+                # be sure to set the text properly to handle
+                # http://dev.w3.org/html5/spec-preview/global-attributes.html#the-dir-attribute
+                # if there are both ltr and rtl charas, force to ltr
+                if elem.text is not None:
+                    #for child in elem.itertext():
+                    #    print(child)
+                    elem.set('dir', self._check_true_dir(elem.text))
+                    #print("---", elem.tag, list(elem.iter()))
+
+    # http://stackoverflow.com/questions/17684990/python-testing-for-utf-8-character-in-string
+    def _check_true_dir(self, text):
+        is_rtl = False
+        is_ltr = False
+        last_inline_html_char_pos = text.rfind(">")
+        if last_inline_html_char_pos > -1:
+            it_here = text[last_inline_html_char_pos:]
+        else:
+            it_here = text
+        for ch in it_here:
+            res = UD.bidirectional(ch)
+            if res in {'R', 'AL'}:
+                is_rtl = True
+            elif res == 'L':
+                is_ltr = True
+        #print(it_here, is_rtl, is_ltr)
+        if is_ltr and is_rtl:
+            return 'ltr'
+        else:
+            return 'auto'
+
+
+class AutoTextDirectionExtension(Extension):
+
+    def extendMarkdown(self, md, md_globals):
+        md.registerExtension(self)
+        self.processor = AutoTextDirectionTreeprocessor()
+        self.processor.md = md
+        md.treeprocessors.add('autotextdir', self.processor, '_begin')
+
+
+def makeExtension(configs={}):
+    return AutoTextDirectionExtension(configs=configs)
+
