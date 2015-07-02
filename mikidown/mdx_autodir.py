70ee73f392d76578e7be448e91e694987dab2977
ShadowKyogre <shadowkyogre@aim.com>
2015-07-02 11:24:09 -0700

    Don't forget to default to auto when the primary text is held in the children, like in the case of ul, ol, etc. Don't also forget to ignore quoted text when determining the primary direction of the piece.

--- mikidown/mdx_autodir.py
+++ mikidown/mdx_autodir.py
@@ -23,29 +23,42 @@ class AutoTextDirectionTreeprocessor(Treeprocessor):
                 # be sure to set the text properly to handle
                 # http://dev.w3.org/html5/spec-preview/global-attributes.html#the-dir-attribute
                 # if there are both ltr and rtl charas, force to ltr
+                #print(elem.tag)
                 if elem.text is not None:
                     #for child in elem.itertext():
                     #    print(child)
                     elem.set('dir', self._check_true_dir(elem.text))
                     #print("---", elem.tag, list(elem.iter()))
+                else:
+                    elem.set('dir', 'auto')
+                #print("---")
 
     # http://stackoverflow.com/questions/17684990/python-testing-for-utf-8-character-in-string
     def _check_true_dir(self, text):
         is_rtl = False
         is_ltr = False
+        quoted_text = False
+
         last_inline_html_char_pos = text.rfind(">")
         if last_inline_html_char_pos > -1:
-            it_here = text[last_inline_html_char_pos:]
+            it_here = text[last_inline_html_char_pos+1:]
         else:
             it_here = text
+
         for ch in it_here:
             res = UD.bidirectional(ch)
-            if res in {'R', 'AL'}:
+            if ch == '"':
+                quoted_text = not quoted_text
+            elif not quoted_text and res in {'R', 'AL'}:
                 is_rtl = True
-            elif res == 'L':
+            elif not quoted_text and res == 'L':
                 is_ltr = True
-        #print(it_here, is_rtl, is_ltr)
-        if is_ltr and is_rtl:
+
+        #print(text, it_here, is_rtl, is_ltr)
+
+        if is_rtl:
+            return 'rtl'
+        elif is_ltr:
             return 'ltr'
         else:
             return 'auto'
